f4b61eba1a16c7a25285c8f74eace9eaa2648f6a
urdevi <urdevi@roehampton.ac.uk>
2024-12-13 13:25:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 document.getElementById('toggle-button').addEventListener('click', function () {
-    const content = document.getElementById('dynamic-text');
-
-    // Check current content and toggle to new content
-    if (content.textContent === 'This is the default content.') {
-        content.textContent = 'Here is the new content you wanted to see!';
-    } else {
-        content.textContent = 'This is the default content.';
-    }
+    const message = document.getElementById('message');
+    message.textContent = 'You clicked the button!';
+    message.style.color = 'blac';
 });
