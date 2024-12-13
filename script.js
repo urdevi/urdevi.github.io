3c5b151c101f24ed2a0681104036dc25a0f91005
urdevi <urdevi@roehampton.ac.uk>
2024-12-13 11:42:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,4 @@ document.getElementById('action-button').addEventListener('click', function () {
     const message = document.getElementById('message');
     message.textContent = 'The following content will show what have been done to achieve the results which has been required';
     message.style.color = 'green';
-    <section>
-            <h2>Here is an example image:</h2>
-            <img src="Images/download.jpeg" alt="A beautiful example image" width="1000" />
-            <p>The image above is displayed using the <code>&lt;img&gt;</code> tag.</p>
-        </section>
 });
